700449dad346847172eadbf939652b1f8c9cc408
violeta <violetaagudelo1@gmail.com>
2024-10-13 13:19:27 -0500

    agregar hospital

--- Front-End/Back-End/app.py
+++ Front-End/Back-End/app.py
@@ -5,6 +5,8 @@ app = Flask(__name__, static_folder='../static', template_folder="../templates")
 app.secret_key = 'clave'
 
 
+
+
 @app.route("/")
 def inicio():
     return render_template("IniciarSesion.html")
@@ -32,25 +34,51 @@ def inicio_sesion():
     
 
 
-
 @app.route('/menu_admin')
 def menu_admin():
     return render_template("MenuAdmi.html")
 
 
-@app.route('/catalogo_hospitales', methods=['GET'])
+@app.route('/catalogo_hospitales', methods=["GET"])
 def catalogo_hospitales():
     return render_template("CatalogoHospitales.html")
 
 
-@app.route('/ver_hospital', methods=['GET'])
+@app.route('/ver_hospital', methods=["GET"])
 def ver_hospital():
     return render_template("VerHospital.html") 
 
+@app.route('/gestionar_hospitales', methods=["GET", "POST"])
+def gestionar_hospitales():
+    return render_template("GestionHospitales.html") 
 
-@app.route('/ver_campañas', methods=['GET'])
+
+@app.route('/ver_campañas', methods=["GET"])
 def ver_campañas():
     return render_template("VerCampañas.html") 
+
+
+#los hospitales se agregan en una lista hospitales
+hospitales=[] 
+@app.route('/agregar_hospital', methods=["GET", "POST"])
+def agregar_hospital():
+    if request.method == "POST":
+        Nombre= request.form["nombre"]
+        Direccion= request.form["direccion"]
+        Contacto= request.form["contacto"]
+        Horario= request.form["horario"]
+        Estado= request.form["estado"]
+        
+        
+        nuevo_hodpital={
+            "Nombre": nombre, "Direccion": direccion, "Contacto": contacto, "Horario": horario, "Estado": estado
+        }
+        
+        hospitales.append(nuevo_hodpital)
+            
+        
+    return render_template("AgregarHospital.html") 
+
 
 
 
