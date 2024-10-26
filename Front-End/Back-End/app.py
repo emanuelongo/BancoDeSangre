2d0d3b7a16b041eb000bb584f43211cdc4ca0546
violeta <violetaagudelo1@gmail.com>
2024-10-26 10:46:18 -0500

    ver hospitales

--- Front-End/Back-End/app.py
+++ Front-End/Back-End/app.py
@@ -5,8 +5,6 @@ app = Flask(__name__, static_folder='../static', template_folder="../templates")
 app.secret_key = 'clave'
 
 
-
-
 @app.route("/")
 def inicio():
     return render_template("IniciarSesion.html")
@@ -33,36 +31,36 @@ def inicio_sesion():
     
     
 
-
 @app.route('/menu_admin')
 def menu_admin():
     return render_template("MenuAdmi.html")
 
-
-@app.route('/catalogo_hospitales', methods=["GET"])
-def catalogo_hospitales():
-    return render_template("CatalogoHospitales.html")
-
-
-@app.route('/ver_hospital', methods=["GET"])
-def ver_hospital():
-    return render_template("VerHospital.html") 
-
-@app.route('/gestionar_hospitales', methods=["GET", "POST"])
-def gestionar_hospitales():
-    return render_template("GestionHospitales.html") 
-
-
-@app.route('/ver_campañas', methods=["GET"])
-def ver_campañas():
-    return render_template("VerCampañas.html") 
-
+num_hospital=0
 
 #los hospitales se agregan en una lista hospitales
 hospitales=[] 
+
+hospital1 = {   
+    "id": num_hospital,
+    "Nombre": "Hospital General de Medellín",
+    "Direccion": "Cra. 48 #32-102, La Candelaria, Medellin",
+    "Contacto": "324 789 253",  
+    "Horario": "24 horas",
+    "Estado": "habilitado"
+}
+
+    
+hospitales.append(hospital1)
+
+
 @app.route('/agregar_hospital', methods=["GET", "POST"])
 def agregar_hospital():
-    if request.method == "POST":
+    global num_hospital
+    
+    
+        
+    if request.method == "POST": #cuando se pulsa el boton de agregar hospital
+        
         nombre = request.form.get('nombre')
         direccion = request.form.get('direccion')
         contacto = request.form.get('contacto')
@@ -70,23 +68,68 @@ def agregar_hospital():
         estado = request.form.get('estado')
         
         
-        nuevo_hodpital={
-            "Nombre": nombre, "Direccion": direccion, "Contacto": contacto, "Horario": horario, "Estado": estado
+        nuevo_hospital={
+            "id": num_hospital, "Nombre": nombre, "Direccion": direccion, "Contacto": contacto, "Horario": horario, "Estado": estado
         }
         
-        hospitales.append(nuevo_hodpital)
-        print(hospitales) #para probar que si se estan guardando
-            
+        hospitales.append(nuevo_hospital)
+        num_hospital += 1
         
+        print(hospitales) #para probar que si se estan guardando   
     return render_template("AgregarHospital.html") 
+
+
+@app.route('/ver_hospital2/<int:hospital_id>', methods=["GET"])
+def ver_hospital2(hospital_id):
+    # Buscar el hospital correspondiente por id
+    hospital = None  
+    for h in hospitales:
+        if h['id'] == hospital_id:
+            hospital = h  # Asigna el hospital encontrado
+
+    if hospital is None:
+        return "Hospital no encontrado", 404  # Manejar caso en que no se encuentra el hospital
+
+    return render_template("verhospital2.html", hospital=hospital)
+
+
+
+
+@app.route('/ver_hospital', methods=["GET"])
+def ver_hospital():
+    return render_template("VerHospital.html")
+
+
+
+
+
+@app.route('/gestionar_hospitales', methods=["GET", "POST"])
+def gestionar_hospitales():
+    return render_template("GestionHospitales.html", hospitales =  hospitales) #se pasa la lista de hospitales para iterar y mostrarlos
+
+
+
+@app.route('/catalogo_hospitales', methods=["GET"])
+def catalogo_hospitales():
+    return render_template("CatalogoHospitales.html", hospitales= hospitales) #se pasa la lista de hospitales para iterar y mostrarlos
+
 
 @app.route('/editar_hospital', methods=["GET", "POST"])
 def editar_hospital():
+    if request.method == "POST":
+        pass
+    
     return render_template("EditarHospitales.html") 
+
+
 
 @app.route('/eliminar_hospital', methods=["GET", "POST"])
 def eliminar_hospital():
     return render_template("EliminarHospitales.html") 
+
+@app.route('/ver_campañas', methods=["GET"])
+def ver_campañas():
+    return render_template("VerCampañas.html") 
 
 
 if __name__ == "__main__":
