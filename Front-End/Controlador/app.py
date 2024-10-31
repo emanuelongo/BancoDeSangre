64c9c20ac37878c1547eeea3d910eec86963b2f9
Lau <laurabedoyacoronado@gmail.com>
2024-10-31 00:23:34 -0500

    registrar

--- Front-End/Controlador/app.py
+++ Front-End/Controlador/app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 import psycopg2
 from insertar2 import crear_campana  
+from insertar import insertar_usuario
+
+
 
 
 app = Flask(__name__, static_folder='../static', template_folder="../templates")
@@ -168,6 +171,32 @@ def agregar_campaña():
         return redirect(url_for('gestionar_campañas'))  
   
     return render_template("AgregarCampaña.html")
+
+@app.route('/registrar', methods=['GET', 'POST'])
+def registrar():
+    if request.method == 'POST':
+        # Recibe los datos del formulario
+        tipo_usuario = request.form['tipo_usuario']
+        nombre = request.form['nombre']
+        tipo_documento = request.form['tipo_documento']
+        numero_documento = request.form['numero_documento']
+        fecha_nacimiento = request.form['fecha_nacimiento']
+        email = request.form['email']
+        password = request.form['password']
+        
+        # Llama a la función de inserción
+        exito, mensaje = insertar_usuario(tipo_usuario, nombre, tipo_documento, numero_documento, fecha_nacimiento, email, password)
+        
+        # Devuelve mensaje de éxito o error
+        if exito:
+            return "¡Registro exitoso!"
+        else:
+            return f"Error en el registro: {mensaje}"
+    else:
+        # Si es GET, muestra el formulario de registro
+        return render_template('Registrarse.html')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
