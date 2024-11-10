7b8fca44bd448674a0333e3728e181c9f4b020c4
violeta <violetaagudelo1@gmail.com>
2024-11-10 13:30:05 -0500

    campañas realizadas

--- Front-End/Controlador/app.py
+++ Front-End/Controlador/app.py
@@ -150,15 +150,20 @@ def eliminar_hospital():
 
 @app.route('/ver_campañas', methods=["GET"])
 def ver_campañas():
+    campañas_activas=[]
     campañas = obtener_campañas()
+    
+    for campaña in campañas:
+        if campaña[9] == False:
+            campañas_activas.append(campaña)
     
     campañasRealizadas= []
     for campaña in campañas:
-        if campaña[8]==True:
+        if campaña[9]==True:
             campañasRealizadas.append(campaña)
     print(campañasRealizadas)
     
-    return render_template("VerCampañas.html", campañas= campañas, campañasRealizadas= campañasRealizadas) 
+    return render_template("VerCampañas.html", campañas_activas= campañas_activas, campañasRealizadas= campañasRealizadas) 
 
 
 
